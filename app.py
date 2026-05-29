2b6d7beaea8ec1452363e0223fa0560857a00c48
Muskan2771 <155989406+Muskan2771@users.noreply.github.com>
2026-05-29 18:21:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -267,4 +267,5 @@ def record_sale():
 # RUN
 # ------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
